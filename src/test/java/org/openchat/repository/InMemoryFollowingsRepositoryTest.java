6914c15f3f530215e834fb497a236211fdd93168
Rafael Heredia Bastidas <heredia.developer@gmail.com>
2019-11-27 01:19:05 -0300

    add one more unit test

--- src/test/java/org/openchat/repository/InMemoryFollowingsRepositoryTest.java
+++ src/test/java/org/openchat/repository/InMemoryFollowingsRepositoryTest.java
@@ -1,8 +1,8 @@
 package org.openchat.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openchat.repository.InMemoryFollowingsRepository;
 import org.openchat.usercases.FollowingRequest;
 
 import java.util.List;
@@ -10,11 +10,17 @@ import java.util.Optional;
 
 class InMemoryFollowingsRepositoryTest {
 
+    private InMemoryFollowingsRepository inMemoryFollowingsRepository;
+
+    @BeforeEach
+    void setUp() {
+        inMemoryFollowingsRepository = new InMemoryFollowingsRepository();
+    }
+
     @Test
     void given_the_following_request_it_will_add_the_new_relationship() {
         //arrange
         FollowingRequest followingRequest = new FollowingRequest("test_followeeId", "test_followingId");
-        InMemoryFollowingsRepository inMemoryFollowingsRepository = new InMemoryFollowingsRepository();
 
         //act
         inMemoryFollowingsRepository.addNewFollowing(followingRequest);
@@ -25,13 +31,26 @@ class InMemoryFollowingsRepositoryTest {
     }
 
     @Test
-    void given_the_following_request_when_adding_two_it_will_get_the_following_list_for_the_following_user_id() {
+    void given_the_following_request_with_a_relationship_that_does_not_exist_it_will_not_add_the_new_relationship() {
         //arrange
-        InMemoryFollowingsRepository inMemoryFollowingsRepository = new InMemoryFollowingsRepository();
+        FollowingRequest followingRequest = new FollowingRequest("test_followeeId", "test_followingId");
 
         //act
-        inMemoryFollowingsRepository.addNewFollowing(new FollowingRequest("test_followeeId1", "test_followingId"));
-        inMemoryFollowingsRepository.addNewFollowing(new FollowingRequest("test_followeeId2", "test_followingId"));
+        Optional<String> ifExistOptional = inMemoryFollowingsRepository.checkIfExist(followingRequest);
+
+        //assert
+        Assertions.assertThat(ifExistOptional.isPresent()).isFalse();
+    }
+
+    @Test
+    void given_the_following_request_when_adding_two_it_will_get_the_following_list_for_the_following_user_id() {
+        //arrange
+        FollowingRequest followingRequest = new FollowingRequest("test_followeeId1", "test_followingId");
+        FollowingRequest followingRequest1 = new FollowingRequest("test_followeeId2", "test_followingId");
+
+        //act
+        inMemoryFollowingsRepository.addNewFollowing(followingRequest);
+        inMemoryFollowingsRepository.addNewFollowing(followingRequest1);
         List<String> followings = inMemoryFollowingsRepository.getAll("test_followingId");
 
         //assert
